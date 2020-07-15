169464ad79bac198ec8814a120d3b7e1101aaa36
lucy <lucy@lunaticmc.net>
2020-07-15 17:01:37 +0100

    copyright, also broke the init string

--- src/main/java/xyz/poulton/lunaticnicks/api/db/DatabaseConnector.java
+++ src/main/java/xyz/poulton/lunaticnicks/api/db/DatabaseConnector.java
@@ -1,6 +1,18 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
+//    Copyright © Lucy Poulton 2020.
+//    This file is part of LunaticNicks.
+//
+//    LunaticNicks is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    LunaticNicks is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LunaticNicks. If not, see <https://www.gnu.org/licenses/>.
 
 package xyz.poulton.lunaticnicks.api.db;
 
@@ -21,7 +33,7 @@ public class DatabaseConnector
     
     public DatabaseConnector(final String hostname, final String db, final int port, final String username, final String password) throws SQLException, ClassNotFoundException {
         this.connection = null;
-        this.dbInitString = "CREATE TABLE IF NOT EXISTS `" + db + "`.`lunaticnicks` (\n  `uuid` VARCHAR(36) NOT NULL,\n  `nick` TEXT NULL,\n  PRIMARY KEY (`uuid`));";
+        this.dbInitString = "CREATE DATABASE IF NOT EXISTS " + db + "; CREATE TABLE IF NOT EXISTS `" + db + "`.`lunaticnicks` (\n  `uuid` VARCHAR(36) NOT NULL,\n  `nick` TEXT NULL,\n  PRIMARY KEY (`uuid`));";
         this.init(hostname, db, port, username, password);
     }
     
--- src/main/java/xyz/poulton/lunaticnicks/bungee/command/NickCommand.java
+++ src/main/java/xyz/poulton/lunaticnicks/bungee/command/NickCommand.java
@@ -1,6 +1,18 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
+//    Copyright © Lucy Poulton 2020.
+//    This file is part of LunaticNicks.
+//
+//    LunaticNicks is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    LunaticNicks is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LunaticNicks. If not, see <https://www.gnu.org/licenses/>.
 
 package xyz.poulton.lunaticnicks.bungee.command;
 
